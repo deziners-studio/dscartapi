502c86151454a518526c0d4978f1da59bfe1f6e0
Kalpak Palekar <kalpak.palekar@gmail.com>
2017-12-05 23:30:35 +0530

    updated products query

--- routes/products/index.js
+++ routes/products/index.js
@@ -7,11 +7,13 @@ router.get('/', (req, res, next) => {
     pool.getConnection((err, connection) => {
         if (err) throw err;
 
-        connection.query('SELECT * FROM products', (err, results, fields) => {
+        connection.query(
+          'SELECT `products`.*, `brands`.`brand_id`, `brands`.`brand_title`, `image_gallery`.`image_gallery_id`, `image_gallery`.`image_gallery_title`, `tags`.`tag_id`, `tags`.`tag_title`, `tags`.`tag_count`, `categories`.`category_id`, `categories`.`category_title`, `gst`.`gst_id`, `gst`.`gst_title` FROM `products` , `brands` , `image_gallery` , `tags` , `categories` , `gst`',
+          (err, results, fields) => {
             if (err) throw err;
-            
+
             res.json(results);
-            connection.release();  
+            connection.release();
         });
     });
 });
